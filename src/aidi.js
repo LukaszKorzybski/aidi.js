0b244886018d37a71799f04602be8e35c5f875da
Lukasz Korzybski <lukasz.korzybski@gmail.com>
2015-02-02 09:52:32 +0000

    added inject function

--- src/aidi.js
+++ src/aidi.js
@@ -2,32 +2,34 @@ var Aidi = function() {
 	'use strict';
 
 	var providers = {},
-		services = {},
+		services = {},		
 
 		functionParamsRegex = /^[^\(]+\(([^\)]*)\)\s+{/;
 
-	var resolveDependencies = function(serviceName, dependencies) {
+	var resolveDependencies = function(dependencies, componentName) {
 		return dependencies.map(function(dep) {
 			var service = getServiceInstance(dep);
 			if (!service) {
-				throw new Error("Cannot resolve dependency: " + dep + ", for provider: " + serviceName);
+				throw new Error("Cannot resolve dependency: " + dep + 
+					(componentName ? ", for component: " + componentName : ""));
 			}
 			return service;
 		});
 	};
 
-	var identifyDependencies = function(serviceName, provider, _dependencies) {
-		var dependencies = _dependencies ? _dependencies : provider.__inject__;
+	var identifyDependencies = function(component, _dependencies, componentName) {
+		var dependencies = _dependencies ? _dependencies : component.__inject__;
 
 		if (!dependencies) {
-			var paramsMatch = provider.toString().match(functionParamsRegex);
+			var paramsMatch = component.toString().match(functionParamsRegex);
 
 			if (paramsMatch && paramsMatch.length === 2) {
 				dependencies = paramsMatch[1].split(',')
 					.map(function(p) { return p.trim(); })
 					.filter(function(p) { return p !== ""; });
 			} else {
-				throw new Error("Error when parsing provider function's parameters list, provider: " + serviceName);
+				throw new Error("Error when identifying dependencies. Parsing function's parameters list failed." + 
+					(componentName ? " component: " + componentName : ""));
 			}
 		}
 
@@ -35,7 +37,7 @@ var Aidi = function() {
 	}
 
 	var registerServiceProvider = function(name, provider, _dependencies) {
-		var dependencies = identifyDependencies(name, provider, _dependencies);
+		var dependencies = identifyDependencies(provider, _dependencies, name);
 
 		if (providers[name] === undefined) {
 			provider.__name__ = name;
@@ -50,8 +52,8 @@ var Aidi = function() {
 	var createServiceInstance = function(name) {
 		var provider = providers[name];
 		if (provider) {
-			var resolvedDependencies = resolveDependencies(name, provider.__inject__);
-			return provider.apply(this, resolvedDependencies);
+			var resolvedDeps = resolveDependencies(provider.__inject__, name);
+			return provider.apply(this, resolvedDeps);
 		} else {
 			throw new Error('Provider ' + name + ' not found');
 		}
@@ -66,15 +68,21 @@ var Aidi = function() {
 
 	return {
 		providers: providers,
-		services: services,		
+		services: services,
 
-		service: function(name, provider, _dependencies) {
+		service: function(name, provider, dependencies) {
 			if (arguments.length === 1) {				
 				return getServiceInstance(name);
 			} else {
-				registerServiceProvider(name, provider, _dependencies);
+				registerServiceProvider(name, provider, dependencies);
 				return this;
 			}						
+		},
+		inject: function(component, _dependencies) {
+			var dependencies = identifyDependencies(component, _dependencies),
+				resolvedDeps = resolveDependencies(dependencies);
+
+			return component.apply(undefined, resolvedDeps);
 		}		
 	};
 };

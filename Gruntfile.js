b488c3c74e9f215f2188e65a818d38dc5ae1e8a8
Lukasz Korzybski <lukasz.korzybski@gmail.com>
2015-04-14 14:15:21 +0100

    added Karma test runner and removed simple static runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,25 @@ module.exports = function(grunt) {
 		jshint: {
 			src: ['src/*.js', 'test/**/*.js']
 		},
+		karma: {
+			options: {
+				frameworks: ['jasmine'],
+				files: ['src/*.js', 'test/specs/*.js'],
+				browsers: ['Chrome']
+			},
+			unit: {
+				options: {
+					autoWatch: true,
+					singleRun: false
+				}
+			},
+			unit_dist: {
+				options: {
+					autoWatch: false,
+					singleRun: true
+				}
+			}
+		},
 		uglify: {
 			aidi: {
 				options: {					
@@ -17,8 +36,11 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-karma');
 
 	grunt.registerTask('analyze', ['jshint']);
-	grunt.registerTask('dist', ['analyze', 'uglify:aidi']);
+	grunt.registerTask('test', ['karma:unit_dist']);
+	grunt.registerTask('test-dev', ['karma:unit']);	
+	grunt.registerTask('dist', ['analyze', 'test', 'uglify:aidi']);
 };
 

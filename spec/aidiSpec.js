37922c82fd4e73b707c2c5608db4c5f55180bd67
Lukasz Korzybski <lukasz.korzybski@gmail.com>
2015-12-10 15:05:56 +0000

    moved unit tests to let statement and strict mode

--- spec/aidiSpec.js
+++ spec/aidiSpec.js
@@ -1,9 +1,11 @@
-var Aidi = require('../src/aidi');
+'use strict';
+
+let Aidi = require('../src/aidi');
 
 describe('aidi', function() {
     'use strict';
 
-    var sut,
+    let sut,
         serviceProvider,
         service1Provider;
 
@@ -28,14 +30,14 @@ describe('aidi', function() {
         });
 
         it('should return instance of a component requested as created by the provider', function() {
-            var service = sut.component('testService');
+            let service = sut.component('testService');
             expect(service.msg).toEqual('test service');
         });
 
         it('should resolve and inject inline defined dependecies when called to register new component', function() {
             sut.component('testService1', service1Provider, ['testService']);
 
-            var testService1 = sut.component('testService1');
+            let testService1 = sut.component('testService1');
             expect(testService1.msg).toEqual('test service 1');
         });
 
@@ -54,55 +56,55 @@ describe('aidi', function() {
         });
 
         it('should return the same and only instance of given component when called multiple times', function() {
-            var service1 = sut.component('testService');
-            var service2 = sut.component('testService');
+            let service1 = sut.component('testService');
+            let service2 = sut.component('testService');
 
             expect(service1).toBe(service2);
         });
 
         it("should use provider's __inject__ property to identify dependencies when no inline dependencies were passed", function() {
-            var testProvider = function(testService) {
+            let testProvider = function(testService) {
                 return testService.msg;
             };
             testProvider.__inject__ = ['testService'];
 
             sut.component('testService1', testProvider);
 
-            var testService1 = sut.component('testService1');
+            let testService1 = sut.component('testService1');
             expect(testService1).toEqual('test service');
         });
 
         it("should use provider parameters' names to identify dependencies when no explicit dependencies information is given", function() {
-            var testProvider = function(  testService  ,testService1 ) {
+            let testProvider = function(  testService  ,testService1 ) {
                 return testService.msg + ',' + testService1.msg;
             };
             sut.component('testService1', service1Provider);
             sut.component('testService2', testProvider);
 
-            var testService2 = sut.component('testService2');
+            let testService2 = sut.component('testService2');
             expect(testService2).toEqual('test service,test service 1');
         });
 
         it("should be able to correctly identify dependencies of arrow functions", function() {
-            var testProvider = (testService) => {
+            let testProvider = (testService) => {
                 return testService.msg;
             };
 
             sut.component('testService1', testProvider);
 
-            var testService2 = sut.component('testService1');
+            let testService2 = sut.component('testService1');
             expect(testService2).toEqual('test service');
         });
 
         it("should not fail if provider function doesn't have any parameters", function() {
-            var testService = sut.component('testService');
+            let testService = sut.component('testService');
             expect(testService.msg).toEqual('test service');
         });
     });
 
     describe('inject', function() {
         it('should return result of the function passed', function() {
-            var result = {},
+            let result = {},
                 func = function() { return result; };
 
             expect(sut.inject(func)).toBe(result);
@@ -113,53 +115,53 @@ describe('aidi', function() {
         });
 
         it('should resolve and inject inline defined dependecies', function() {
-            var func = function(testService) {
+            let func = function(testService) {
                 return testService.msg;
             };
 
-            var result = sut.inject(func, ['testService']);
+            let result = sut.inject(func, ['testService']);
             expect(result).toEqual('test service');
         });
 
         it("should use component's __inject__ property to identify dependencies when no inline dependencies were passed", function() {
-            var func = function(testService) {
+            let func = function(testService) {
                 return testService.msg;
             };
             func.__inject__ = ['testService'];
 
-            var result = sut.inject(func);
+            let result = sut.inject(func);
             expect(result).toEqual('test service');
         });
 
         it("should be able to correctly identify dependencies of arrow functions", function() {
-          var func = (testService) => {
+          let func = (testService) => {
               return testService.msg;
           };
           func.__inject__ = ['testService'];
 
-          var result = sut.inject(func);
+          let result = sut.inject(func);
           expect(result).toEqual('test service');
         });
 
         it("should use component parameters' names to identify dependencies when no explicit dependencies information is given", function() {
-            var func = function(  testService  ,testService1 ) {
+            let func = function(  testService  ,testService1 ) {
                 return testService.msg + ',' + testService1.msg;
             };
             sut.component('testService1', service1Provider);
 
-            var result = sut.inject(func);
+            let result = sut.inject(func);
             expect(result).toEqual('test service,test service 1');
         });
 
         it('should support injecting dependencies into objects', function() {
-            var obj = {};
+            let obj = {};
             sut.inject(obj, ['testService']);
             expect(obj.testService).toBeDefined();
             expect(obj.testService.msg).toEqual('test service');
         });
 
         it('should support injecting into objects dependencies defined with __inject__ property', function() {
-            var obj1 = { __inject__: ['testService'] };
+            let obj1 = { __inject__: ['testService'] };
             sut.inject(obj1);
             expect(obj1.testService).toBeDefined();
             expect(obj1.testService.msg).toEqual('test service');
@@ -168,10 +170,10 @@ describe('aidi', function() {
 
     describe('service', function() {
         it('should be an alias for the component method', function() {
-            var service = function(dependency) {};
+            let service = function(dependency) {};
 
             sut.component = spyOn(sut, 'component').and.returnValue(sut);
-            var result = sut.service('testService2', service, ['dependency']);
+            let result = sut.service('testService2', service, ['dependency']);
 
             expect(sut.component.calls.count()).toEqual(1);
             expect(sut.component.calls.argsFor(0)).toEqual(['testService2', service, ['dependency']]);

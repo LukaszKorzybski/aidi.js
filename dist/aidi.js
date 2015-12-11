0d50cc83ded80ba44a6517dc4face1caf89dfd1c
Lukasz Korzybski <lukasz.korzybski@gmail.com>
2015-12-11 13:48:06 +0000

    migrated to ES6 classes

--- dist/aidi.js
+++ dist/aidi.js
@@ -1,109 +1,127 @@
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.default = Aidi;
-function Aidi() {
 
-    var functionParamsRegex = /^[^\(]*\(([^\)]*)\)\s*(=>)?\s*{/;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    var providers = {};
-    var components = {};
+var Aidi = (function () {
+    function Aidi() {
+        _classCallCheck(this, Aidi);
 
-    this.providers = providers;
-    this.components = components;
+        this.providers = {};
+        this.components = {};
+    }
 
-    this.component = function (name, provider, dependencies) {
-        if (arguments.length === 1) {
-            return getComponentInstance(name);
-        } else {
-            registerComponentProvider(name, provider, dependencies);
-            return this;
-        }
-    };
-
-    this.service = function () {
-        return this.component.apply(this, arguments);
-    };
-
-    this.inject = function (component, _dependencies) {
-        var dependencies = identifyDependencies(component, _dependencies),
-            resolvedDeps = resolveDependencies(dependencies);
-
-        return applyDependencies(component, dependencies, resolvedDeps);
-    };
-
-    var resolveDependencies = function resolveDependencies(dependencies, componentName) {
-        return (dependencies || []).map(function (dep) {
-            var dependency = getComponentInstance(dep);
-            if (!dependency) {
-                throw new Error('Cannot resolve dependency: ' + dep + ' for component: ' + componentName + ' ');
-            }
-            return dependency;
-        });
-    };
-
-    var identifyDependencies = function identifyDependencies(component, _dependencies, componentName) {
-        var dependencies = _dependencies || component.__inject__;
-
-        if (typeof component === 'function' && !dependencies) {
-            var paramsMatch = component.toString().match(functionParamsRegex);
-
-            if (paramsMatch && paramsMatch.length > 1) {
-                dependencies = paramsMatch[1].split(',').map(function (p) {
-                    return p.trim();
-                }).filter(function (p) {
-                    return p !== "";
-                });
+    _createClass(Aidi, [{
+        key: 'component',
+        value: function component(name, provider, dependencies) {
+            if (arguments.length === 1) {
+                return getComponentInstance.call(this, name);
             } else {
-                throw new Error("Error when identifying dependencies. Parsing " + ('function\'s parameters list failed. component: ' + componentName));
+                registerComponentProvider.call(this, name, provider, dependencies);
+                return this;
             }
         }
-
-        return dependencies || [];
-    };
-
-    var registerComponentProvider = function registerComponentProvider(name, provider, _dependencies) {
-        var dependencies = identifyDependencies(provider, _dependencies, name);
-
-        if (providers[name] === undefined) {
-            provider.__name__ = name;
-            provider.__inject__ = dependencies || [];
-
-            providers[name] = provider;
-        } else {
-            throw new Error('Provider ' + name + ' is already registered');
+    }, {
+        key: 'service',
+        value: function service() {
+            return this.component.apply(this, arguments);
         }
-    };
+    }, {
+        key: 'inject',
+        value: function inject(component, _dependencies) {
+            var dependencies = identifyDependencies.call(this, component, _dependencies),
+                resolvedDeps = resolveDependencies.call(this, dependencies);
 
-    var createComponentInstance = function createComponentInstance(name) {
-        var provider = providers[name];
-        if (provider) {
-            var resolvedDeps = resolveDependencies(provider.__inject__, name);
-            return provider.apply(undefined, resolvedDeps);
-        } else {
-            throw new Error('Provider ' + name + ' not found');
+            return applyDependencies.call(this, component, dependencies, resolvedDeps);
         }
-    };
+    }]);
 
-    var getComponentInstance = function getComponentInstance(name) {
-        if (components[name] === undefined) {
-            components[name] = createComponentInstance(name);
-        }
-        return components[name];
-    };
+    return Aidi;
+})();
 
-    var applyDependencies = function applyDependencies(component, dependencies, resolvedDeps) {
-        if (typeof component === 'function') {
-            return component.apply(undefined, resolvedDeps);
-        } else {
-            resolvedDeps.forEach(function (dep, i) {
-                component[dependencies[i]] = dep;
-            });
-            return component;
-        }
-    };
-};
+exports.default = Aidi;
+;
 
 exports.Aidi = Aidi;
+
+// private
+
+var functionParamsRegex = /^[^\(]*\(([^\)]*)\)\s*(=>)?\s*{/;
+
+var resolveDependencies = function resolveDependencies(dependencies, componentName) {
+    var _this = this;
+
+    return (dependencies || []).map(function (dep) {
+        var dependency = getComponentInstance.call(_this, dep);
+        if (!dependency) {
+            throw new Error('Cannot resolve dependency: ' + dep + ' for component: ' + componentName + ' ');
+        }
+        return dependency;
+    });
+};
+
+var identifyDependencies = function identifyDependencies(component, _dependencies, componentName) {
+    var dependencies = _dependencies || component.__inject__;
+
+    if (typeof component === 'function' && !dependencies) {
+        var paramsMatch = component.toString().match(functionParamsRegex);
+
+        if (paramsMatch && paramsMatch.length > 1) {
+            dependencies = paramsMatch[1].split(',').map(function (p) {
+                return p.trim();
+            }).filter(function (p) {
+                return p !== "";
+            });
+        } else {
+            throw new Error("Error when identifying dependencies. Parsing " + ('function\'s parameters list failed. component: ' + componentName));
+        }
+    }
+
+    return dependencies || [];
+};
+
+var registerComponentProvider = function registerComponentProvider(name, provider, _dependencies) {
+    var dependencies = identifyDependencies.call(this, provider, _dependencies, name);
+
+    if (this.providers[name] === undefined) {
+        provider.__name__ = name;
+        provider.__inject__ = dependencies || [];
+
+        this.providers[name] = provider;
+    } else {
+        throw new Error('Provider ' + name + ' is already registered');
+    }
+};
+
+var createComponentInstance = function createComponentInstance(name) {
+    var provider = this.providers[name];
+    if (provider) {
+        var resolvedDeps = resolveDependencies.call(this, provider.__inject__, name);
+        return provider.apply(undefined, resolvedDeps);
+    } else {
+        throw new Error('Provider ' + name + ' not found');
+    }
+};
+
+var getComponentInstance = function getComponentInstance(name) {
+    if (this.components[name] === undefined) {
+        this.components[name] = createComponentInstance.call(this, name);
+    }
+    return this.components[name];
+};
+
+var applyDependencies = function applyDependencies(component, dependencies, resolvedDeps) {
+    if (typeof component === 'function') {
+        return component.apply(undefined, resolvedDeps);
+    } else {
+        resolvedDeps.forEach(function (dep, i) {
+            component[dependencies[i]] = dep;
+        });
+        return component;
+    }
+};
--- src/aidi.js
+++ src/aidi.js
@@ -1,104 +1,106 @@
 'use strict';
 
-export default function Aidi() {
+export default class Aidi {
 
-  const functionParamsRegex = /^[^\(]*\(([^\)]*)\)\s*(=>)?\s*{/;
+  constructor() {
+      this.providers = {};
+      this.components = {};
+  };
 
-  let providers = {};
-  let components = {};
-
-  this.providers = providers;
-  this.components = components;
-
-  this.component = function(name, provider, dependencies) {
+  component(name, provider, dependencies) {
       if (arguments.length === 1) {
-          return getComponentInstance(name);
+          return this::getComponentInstance(name);
       } else {
-          registerComponentProvider(name, provider, dependencies);
+          this::registerComponentProvider(name, provider, dependencies);
           return this;
       }
   };
 
-  this.service = function() {
+  service() {
       return this.component.apply(this, arguments);
   };
 
-  this.inject = function(component, _dependencies) {
-      let dependencies = identifyDependencies(component, _dependencies),
-          resolvedDeps = resolveDependencies(dependencies);
+  inject(component, _dependencies) {
+      let dependencies = this::identifyDependencies(component, _dependencies),
+          resolvedDeps = this::resolveDependencies(dependencies);
 
-      return applyDependencies(component, dependencies, resolvedDeps);
-  };
-
-  let resolveDependencies = function(dependencies, componentName) {
-      return (dependencies || []).map((dep) => {
-          let dependency = getComponentInstance(dep);
-          if (!dependency) {
-              throw new Error(`Cannot resolve dependency: ${dep} for component: ${componentName} `);
-          }
-          return dependency;
-      });
-  };
-
-  let identifyDependencies = function(component, _dependencies, componentName) {
-      let dependencies = _dependencies || component.__inject__;
-
-      if (typeof component === 'function' && !dependencies) {
-          let paramsMatch = component.toString().match(functionParamsRegex);
-
-          if (paramsMatch && paramsMatch.length > 1) {
-              dependencies = paramsMatch[1].split(',')
-                  .map((p) => { return p.trim(); })
-                  .filter((p) => { return p !== ""; });
-          } else {
-              throw new Error("Error when identifying dependencies. Parsing " +
-                `function's parameters list failed. component: ${componentName}`);
-          }
-      }
-
-      return dependencies || [];
-  };
-
-  let registerComponentProvider = function(name, provider, _dependencies) {
-      let dependencies = identifyDependencies(provider, _dependencies, name);
-
-      if (providers[name] === undefined) {
-          provider.__name__ = name;
-          provider.__inject__ = dependencies || [];
-
-          providers[name] = provider;
-      } else {
-          throw new Error(`Provider ${name} is already registered`);
-      }
-  };
-
-  let createComponentInstance = function(name) {
-      let provider = providers[name];
-      if (provider) {
-          let resolvedDeps = resolveDependencies(provider.__inject__, name);
-          return provider.apply(undefined, resolvedDeps);
-      } else {
-          throw new Error(`Provider ${name} not found`);
-      }
-  };
-
-  let getComponentInstance = function(name) {
-      if (components[name] === undefined) {
-          components[name] = createComponentInstance(name);
-      }
-      return components[name];
-  };
-
-  let applyDependencies = function(component, dependencies, resolvedDeps) {
-      if (typeof component === 'function') {
-          return component.apply(undefined, resolvedDeps);
-      } else {
-          resolvedDeps.forEach((dep, i) => {
-              component[dependencies[i]] = dep;
-          });
-          return component;
-      }
+      return this::applyDependencies(component, dependencies, resolvedDeps);
   };
 };
 
 export { Aidi };
+
+
+// private
+
+const functionParamsRegex = /^[^\(]*\(([^\)]*)\)\s*(=>)?\s*{/;
+
+let resolveDependencies = function(dependencies, componentName) {
+    return (dependencies || []).map((dep) => {
+        let dependency = this::getComponentInstance(dep);
+        if (!dependency) {
+            throw new Error(`Cannot resolve dependency: ${dep} for component: ${componentName} `);
+        }
+        return dependency;
+    });
+};
+
+let identifyDependencies = function(component, _dependencies, componentName) {
+    let dependencies = _dependencies || component.__inject__;
+
+    if (typeof component === 'function' && !dependencies) {
+        let paramsMatch = component.toString().match(functionParamsRegex);
+
+        if (paramsMatch && paramsMatch.length > 1) {
+            dependencies = paramsMatch[1].split(',')
+                .map((p) => { return p.trim(); })
+                .filter((p) => { return p !== ""; });
+        } else {
+            throw new Error("Error when identifying dependencies. Parsing " +
+              `function's parameters list failed. component: ${componentName}`);
+        }
+    }
+
+    return dependencies || [];
+};
+
+let registerComponentProvider = function(name, provider, _dependencies) {
+    let dependencies = this::identifyDependencies(provider, _dependencies, name);
+
+    if (this.providers[name] === undefined) {
+        provider.__name__ = name;
+        provider.__inject__ = dependencies || [];
+
+        this.providers[name] = provider;
+    } else {
+        throw new Error(`Provider ${name} is already registered`);
+    }
+};
+
+let createComponentInstance = function(name) {
+    let provider = this.providers[name];
+    if (provider) {
+        let resolvedDeps = this::resolveDependencies(provider.__inject__, name);
+        return provider.apply(undefined, resolvedDeps);
+    } else {
+        throw new Error(`Provider ${name} not found`);
+    }
+};
+
+let getComponentInstance = function(name) {
+    if (this.components[name] === undefined) {
+        this.components[name] = this::createComponentInstance(name);
+    }
+    return this.components[name];
+};
+
+let applyDependencies = function(component, dependencies, resolvedDeps) {
+    if (typeof component === 'function') {
+        return component.apply(undefined, resolvedDeps);
+    } else {
+        resolvedDeps.forEach((dep, i) => {
+            component[dependencies[i]] = dep;
+        });
+        return component;
+    }
+};
